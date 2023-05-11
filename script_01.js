121643dec939469be25f4cad6382dfa7a67c6159
Mertha89 <mertha@live.de>
2023-05-11 09:10:18 +0200

    Kapselung von Codeblöcken

--- script_01.js
+++ script_01.js
@@ -3,3 +3,20 @@
 /***** Funktionen 01 *****/
 // 1. Kapselung von Codeblöcken
 
+
+
+// Funktionsaufruf (call)
+test();
+
+// Funktionsrumpf (body) | callee
+function test()
+{
+    console.log("Hallo");
+}
+
+
+
+
+
+
+
