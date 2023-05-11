7ff3a281b6891a1d9e782b3ed3684f4d6365bc89
Mertha89 <mertha@live.de>
2023-05-11 12:40:48 +0200

    Fertig für heute

--- script_01.js
+++ script_01.js
@@ -52,14 +52,67 @@ function ausgabeNamenParam(firstName)
 
 /***** Funktionen 02c *****/
 // 2c. Mehrere Parameter
-ausgabeNamenParams("Peter", "Zwegert");
-ausgabeNamenParams("Claudia", 'Schmidt')
-ausgabeNamenParams(prompt("Geben sie ihren Vornamen ein:"),prompt("Geben sie ihren Nachnamen ein:"))
+// ausgabeNamenParams("Peter", "Zwegert");
+// ausgabeNamenParams("Claudia", 'Schmidt')
+// ausgabeNamenParams(prompt("Geben sie ihren Vornamen ein:"),prompt("Geben sie ihren Nachnamen ein:"))
 
 function ausgabeNamenParams(firstName, familyName) 
 {
    console.log("Hallo", firstName, familyName,"!");
 
 }
+
+
+
+/***** Funktionen 03a *****/
+// 03a. Vorbereitung
+// Postulat: one function = one job (uncle Bob)
+// SRP single responsibility principle
+
+// ausgabeNamenParams2("Claudia", 'Schmidt')
+
+function ausgabeNamenParams2(firstName, familyName) 
+{
+    // 1. Funktionalität: string composing
+    const GAP = " ";
+    let outputStr = "Hallo" + GAP + firstName + GAP + familyName + "!";
+
+
+    // 2. Funktionalität: string output
+
+   console.log(outputStr);
+
+}
+
+/***** Funktionen 03b *****/
+// Trennen der Funktionalitäten in Funktionen| return
+
+
+output(getString("Max", "Mauser"));
+output (getString (prompt("Gebe deinen Vornamen ein"),prompt("Gebe deinen Nachnamen ein")))
+
+function getString(firstName, familyName) 
+{
+    const GAP = " ";
+    let outputStr = "Hallo" + GAP + firstName + GAP + familyName + "!";
+    return outputStr;  // return sendet Daten an den call zurück
+}
+
+// 2. Funktionalität: string output
+
+// output("Hi")
+// output(prompt("Geben sie ihren Namen ein:"))
+
+function output(outputData) 
+{
+    console.log(outputData);
+    
+
+}
+
+
+
+
+
 
 
